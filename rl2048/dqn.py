174d78d85b714902716c4728487290d8ce82dd84
Vighnesh Birodkar <vighneshbirodkar@gmail.com>
2018-04-28 16:40:03 -0400

    refactoring.

--- rl2048/dqn.py
+++ rl2048/dqn.py
@@ -12,7 +12,8 @@ from torch import nn
 
 from . import config, utils
 from .env import Env2048
-from .exp_buffer import ExperineReplayBuffer, ResampledBuffer
+from .exp_buffer import ExperineReplayBuffer
+from .network import ConvNet
 from .stats import Stats
 from .utils import action_select, copy_data
 
@@ -30,56 +31,6 @@ logging.basicConfig(
 def dump_file(filename, obj):
     with open(filename, 'wb') as f:
         pickle.dump(obj, f)
-
-
-class MLP(nn.Module):
-
-    def __init__(self, input_shape, output_size, num_hidden):
-
-        super(MLP, self).__init__()
-
-        self.num_hidden = num_hidden
-
-        self.input_size = int(np.prod(input_shape))
-        self.net = nn.Sequential(
-            nn.Linear(self.input_size, self.num_hidden),
-            nn.LeakyReLU(0.2, inplace=True),
-            nn.Linear(self.num_hidden, self.num_hidden),
-            nn.LeakyReLU(0.2, inplace=True),
-            nn.Linear(self.num_hidden, self.num_hidden),
-            nn.LeakyReLU(0.2, inplace=True),
-            nn.Linear(self.num_hidden, output_size)
-        )
-
-    def forward(self, state):
-
-        state = state.view(-1, self.input_size)
-        return self.net(state)
-
-
-class ConvNet(nn.Module):
-
-    def __init__(self, input_shape, output_size, num_hidden):
-
-        super(ConvNet, self).__init__()
-
-        self.net = nn.Sequential(
-            nn.Conv2d(1, num_hidden, 2),
-            nn.LeakyReLU(0.2, inplace=True),
-            nn.Conv2d(num_hidden, num_hidden, 2),
-            nn.LeakyReLU(0.2, inplace=True),
-            nn.Conv2d(num_hidden, num_hidden, 2),
-            nn.LeakyReLU(0.2, inplace=True),
-            nn.Conv2d(num_hidden, 4, 1),
-        )
-
-    def forward(self, state):
-
-        state = state.unsqueeze(1)
-        out = self.net(state)
-        out = out.squeeze(3).squeeze(2)
-
-        return out
 
 
 class DQN(object):
--- rl2048/exp_buffer.py
+++ rl2048/exp_buffer.py
@@ -1,6 +1,5 @@
 import json
 import logging
-import random
 from collections import OrderedDict, namedtuple
 
 import numpy as np
@@ -8,6 +7,7 @@ import numpy as np
 from . import config
 
 Step = namedtuple('Step', ['state', 'action', 'reward', 'done', 'next_state'])
+
 
 class FixedFIFO(object):
 
@@ -21,7 +21,7 @@ class FixedFIFO(object):
     def add(self, item):
 
         self.buffer_[self.index] = item
-        self.index = (self.index + 1)%self.size
+        self.index = (self.index + 1) % self.size
         self.valid_samples = min(self.valid_samples + 1, self.size)
 
     def sample(self, size):
@@ -33,6 +33,7 @@ class FixedFIFO(object):
 def round_reward(r):
 
     return int(r*10)/10.0
+
 
 class ExperineReplayBuffer(object):
 
@@ -78,7 +79,6 @@ class ExperineReplayBuffer(object):
 
         n = 10
         rewards = {}
-        state_max = {}
         buffer_ = self.fifo.buffer_
         samples = self.fifo.valid_samples
         rewards = np.array([step.reward for step in buffer_[:samples]])
@@ -93,68 +93,3 @@ class ExperineReplayBuffer(object):
         reward_str = json.dumps(reward_dict, indent=2)
         return logging.info(f"size = {self.fifo.valid_samples} "
                             f"Rewards {reward_str}")
-
-
-class ResampledBuffer(object):
-
-    def __init__(self):
-        conf = config.get_config()
-        self.size = conf.exp_buffer_size
-        self.fifo_size = int(self.size/11.0)
-        self.fifo_map = {-1: FixedFIFO(self.fifo_size)}
-
-        for i in range(10):
-
-            k = round(i/10.0, 2)
-            self.fifo_map[k] = FixedFIFO(self.fifo_size)
-
-    def add(self, state, action, reward, done, next_state):
-
-        step = Step(state=state, action=action, reward=reward, done=done,
-                    next_state=next_state)
-
-        r = round_reward(reward)
-        self.fifo_map[r].add(step)
-
-    def sample(self, batch_size):
-
-        states = []
-        rewards = []
-        actions = []
-        next_states = []
-        finished = []
-
-        for i in range(batch_size):
-
-            non_zero_keys = [k for k in self.fifo_map if 
-                             self.fifo_map[k].valid_samples > 0]
-            k = random.choice(non_zero_keys)
-            fifo = self.fifo_map[k]
-            idx = np.random.randint(0, fifo.valid_samples)
-            step = fifo.buffer_[idx]
-            
-            states.append(step.state)
-            rewards.append(step.reward)
-            actions.append(step.action)
-            finished.append(step.done)
-            next_states.append(step.next_state)
-
-        states = np.array(states)
-        actions = np.array(actions)
-        rewards = np.array(rewards)
-        finished = np.array(finished)
-        next_states = np.array(next_states)
-
-        assert np.max(states) <= 1
-        assert np.max(next_states) <= 1
-
-        return states, actions, rewards, finished, next_states
-
-    def print_stats(self):
-
-        reward_dict = OrderedDict()
-        for k in sorted(self.fifo_map.keys()):
-            reward_dict[k] = self.fifo_map[k].valid_samples
-
-        reward_str = json.dumps(reward_dict, indent=2)
-        return logging.info(f"Rewards {reward_str}")
